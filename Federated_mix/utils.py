4a8e572da3406444054e1e3e94ce17afb7523a53
Luca Villani <45806245+lucavillanigit@users.noreply.github.com>
2022-12-04 15:27:15 +0100

    Update utils.py

--- Federated_mix/utils.py
+++ Federated_mix/utils.py
@@ -20,8 +20,8 @@ def exp_details(args):
     print(f'    Local Batch size   : {args.local_bs}')
     print(f'    Local Epochs       : {args.local_ep}\n')
     print('\nMixed Parameters:')
-    print(f'Alpha Batch : {args.alpha_b}')
-    print(f'Alpha Group : {args.alpha_g}')
+    print(f'    Alpha Batch : {args.alpha_b}')
+    print(f'    Alpha Group : {args.alpha_g}')
     return
 
 def get_dataset(args):
