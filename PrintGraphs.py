4d4984b1589a6563bf91280771c7735f601c97d6
LucaMarcellino <61979109+LucaMarcellino@users.noreply.github.com>
2022-12-06 21:29:46 +0100

    Update PrintGraphs.py

--- PrintGraphs.py
+++ PrintGraphs.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 #---------- NONIID----------#
 
 #---------- Import DataBase ----------#
-dataAvgBN = pd.read_csv("Results\FedAvg_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_0_lr_0.01_mom_0.5_epochs_50.csv")
+dataAvgBN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_0_lr_0.01_mom_0.5_epochs_50.csv")
 
 #---------- Plot the graphs ----------#
 plt.figure(figsize=(10,10))
