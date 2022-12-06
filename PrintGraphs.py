952e50fdf82d9d14af2832256444b7fd01e031c7
LucaMarcellino <61979109+LucaMarcellino@users.noreply.github.com>
2022-12-06 20:56:24 +0100

    Code to print the comparison graphs

---
+++ PrintGraphs.py
@@ -0,0 +1,20 @@
+#---------- Import ----------#
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#---------- NONIID----------#
+
+#---------- Import DataBase ----------#
+dataAvgBN = pd.read_csv("Results\FedAvg_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_0_lr_0.01_mom_0.5_epochs_50.csv")
+
+#---------- Plot the graphs ----------#
+plt.figure(figsize=(10,10))
+plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o", label = 'FedAvg NonIID Batch Norm')
+plt.ylim(bottom = 0)
+plt.title("Comparison between methods With NonIID")
+plt.xlabel("Epochs")
+plt.ylabel("Test Accuracy")
+plt.grid()
+plt.legend(loc="lower right")
+plt.show()
