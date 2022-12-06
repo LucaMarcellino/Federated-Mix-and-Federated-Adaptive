a26e8808a207f09888b2e5537b58143c5dd70d08
LucaMarcellino <61979109+LucaMarcellino@users.noreply.github.com>
2022-12-07 00:28:00 +0100

    NonIID and IID

--- PrintGraphs.py
+++ PrintGraphs.py
@@ -20,3 +20,21 @@ plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
 plt.show()
+
+
+#---------- IID----------#
+
+#---------- Import DataBase ----------#
+dataAvgBN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_1_lr_0.01_mom_0.5_epochs_50.csv")
+
+
+#---------- Plot the graphs ----------#
+plt.figure(figsize=(10,10))
+plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o", label = 'FedAvg NonIID Batch Norm')
+plt.ylim(bottom = 0)
+plt.title("Comparison between methods With IID")
+plt.xlabel("Epochs")
+plt.ylabel("Test Accuracy")
+plt.grid()
+plt.legend(loc="lower right")
+plt.show()
