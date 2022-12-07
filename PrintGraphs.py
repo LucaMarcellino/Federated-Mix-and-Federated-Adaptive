5d7f274ac94e186b2cd3bcac4dc08e7a7f33788a
LucaMarcellino <61979109+LucaMarcellino@users.noreply.github.com>
2022-12-07 10:13:07 +0100

    Add Graphs

--- PrintGraphs.py
+++ PrintGraphs.py
@@ -8,11 +8,13 @@ import matplotlib.pyplot as plt
 #---------- Import DataBase ----------#
 dataAvgBN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_0_lr_0.01_mom_0.5_epochs_50.csv")
 dataMix0505 = pd.read_csv("Results\FedMIX_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.5_alphaG_0.5.csv")
+dataMix0109 = pd.read_csv("Results\FedMIX_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.1_alphaG_0.9.csv")
 
 #---------- Plot the graphs ----------#
 plt.figure(figsize=(10,10))
-plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o", label = 'FedAvg NonIID Batch Norm')
-plt.plot(dataMix0505["Epochs"],dataMix0505["Test accuracy"],marker = "o", label = 'FedMix NonIID alpha_b=0.5 alpha_g=0.5')
+plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o",markersize=3.5,linestyle = "--", label = 'FedAvg NonIID Batch Norm')
+plt.plot(dataMix0505["Epochs"],dataMix0505["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix NonIID alpha_b=0.5 alpha_g=0.5')
+plt.plot(dataMix0109["Epochs"],dataMix0109["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix NonIID alpha_b=0.1 alpha_g=0.9')
 plt.ylim(bottom = 0)
 plt.title("Comparison between methods With NonIID")
 plt.xlabel("Epochs")
@@ -30,7 +32,7 @@ dataAvgBN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Batch Nor
 
 #---------- Plot the graphs ----------#
 plt.figure(figsize=(10,10))
-plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o", label = 'FedAvg IID Batch Norm')
+plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o",markersize=3.5,linestyle = "--", label = 'FedAvg IID Batch Norm')
 plt.ylim(bottom = 0)
 plt.title("Comparison between methods With IID")
 plt.xlabel("Epochs")
