63d6b45c7cdd62a1fb700f6f160c67083fd10cf0
Luca Marcellino <s234262@studenti.polito.it>
2022-12-13 21:26:35 +0100

    Add 0.5-0.5 for adp commeted

--- PrintGraphs.py
+++ PrintGraphs.py
@@ -12,6 +12,7 @@ dataAvgGN = pd.read_csv("Results\FedADP_results\FedAVG_5_local_ep_Norm_Group Nor
 dataMix025075 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.25_alphaG_0.75.csv")
 dataMix0109 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.1_alphaG_0.9.csv")
 dataMix0010 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.0_alphaG_1.0.csv")
+#dataMix0505 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.5_alphaG_0.5.csv")
 
 #---------- Plot the graphs ----------#
 plt.figure(figsize=(10,10))
@@ -20,6 +21,7 @@ plt.plot(dataAvgGN["Epochs"],dataAvgGN["Test accuracy"],marker = "o",markersize=
 plt.plot(dataMix025075["Epochs"],dataMix025075["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.25 alpha_g=0.75')
 plt.plot(dataMix0109["Epochs"],dataMix0109["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.1 alpha_g=0.9')
 plt.plot(dataMix0010["Epochs"],dataMix0010["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.0 alpha_g=1.0')
+#plt.plot(dataMix0505["Epochs"],dataMix0505["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.0 alpha_g=1.0')
 plt.ylim(bottom = 0)
 plt.title("Comparison between methods with IID in FedAdp settings")
 plt.xlabel("Epochs")
@@ -36,6 +38,7 @@ dataAvgGN = pd.read_csv("Results\FedADP_results\FedAVG_5_local_ep_Norm_Group Nor
 #dataMix025075 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.25_alphaG_0.75.csv")
 dataMix0109 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.1_alphaG_0.9.csv")
 dataMix0010 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.0_alphaG_1.0.csv")
+#dataMix0505 = pd.read_csv("Results\FedADP_results\FedMix_5_local_ep_iid_0_lr_0.01_mom_0.5_epochs_50_alphaB_0.5_alphaG_0.5.csv")
 
 #---------- Plot the graphs ----------#
 plt.figure(figsize=(10,10))
@@ -44,6 +47,7 @@ plt.plot(dataAvgGN["Epochs"],dataAvgGN["Test accuracy"],marker = "o",markersize=
 #plt.plot(dataMix025075["Epochs"],dataMix025075["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.25 alpha_g=0.75')
 plt.plot(dataMix0109["Epochs"],dataMix0109["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix NonIID alpha_b=0.1 alpha_g=0.9')
 plt.plot(dataMix0010["Epochs"],dataMix0010["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix NonIID alpha_b=0.0 alpha_g=1.0')
+#plt.plot(dataMix0505["Epochs"],dataMix0505["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.0 alpha_g=1.0')
 plt.ylim(bottom = 0)
 plt.title("Comparison between methods with NonIID in FedAdp settings")
 plt.xlabel("Epochs")
