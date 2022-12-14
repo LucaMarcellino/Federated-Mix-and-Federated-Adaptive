66bd3c0a82541690baca0a769b9151a816fa1e2d
Luca Marcellino <s234262@studenti.polito.it>
2022-12-15 00:26:38 +0100

    try parallel

--- FedGKT_Mix/gkt_client.py
+++ FedGKT_Mix/gkt_client.py
@@ -39,14 +39,16 @@ class DatasetSplit(Dataset):
 class GKTClientTrainer(object):
     def __init__(self, model, device,  train_dataset, test_dataset, idxs, client_index, args):
         #self.model = model
+        """
         if torch.cuda.device_count() > 1:
             print("Let's use", torch.cuda.device_count(), "GPUs!")
             # Batch size should be divisible by number of GPUs
             self.model = tn.DataParallel(model)
             print(next(self.model.parameters()).is_cuda)
         else:
-            self.model = model
-            self.model.to(self.device)
+        """
+        self.model = model
+        self.model.to(self.device)
         self.model_params = self.model.parameters()
         self.device = device
 
