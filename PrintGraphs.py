7c7d67687f22d6803aeacb04db69a6093822a5cc
Luca Marcellino <s234262@studenti.polito.it>
2023-01-10 21:04:41 +0100

    Add images

--- PrintGraphs.py
+++ PrintGraphs.py
@@ -28,6 +28,7 @@ plt.xlabel("Epochs")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with IID in FedAdp settings.png")
 plt.show()
 
 #---------- NONIID----------#
@@ -54,6 +55,7 @@ plt.xlabel("Epochs")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with NonIID in FedAdp settings.png")
 plt.show()
 
 #---------------------------------------------------------------- FEDMIX ----------------------------------------------------------------------------------------#
@@ -80,6 +82,7 @@ plt.title("Comparison between methods with NonIID in FedMix settings ")
 plt.xlabel("Epochs")
 plt.ylabel("Test Accuracy")
 plt.grid()
+plt.savefig("Images/Comparison between methods with NonIID in FedMix settings.png")
 plt.legend(loc="lower right")
 plt.show()
 
@@ -106,33 +109,12 @@ plt.xlabel("Epochs")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with IID in FedMix setting.png")
 plt.show()
 
 
 #---------------------------------------------------------------- FEDGKT 128 local bs----------------------------------------------------------------------------------------#
 
-
-#---------- NONIID----------#
-#---------- Import DataBase ----------#
-#dataAvgBN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Batch Norm_iid_1_lr_0.01_mom_0.5_epochs_50.csv")
-#dataAvgGN = pd.read_csv("Results\FedAVG_results\FedAVG_5_local_ep_Norm_Group Norm_iid_1_lr_0.01_mom_0.5_epochs_50.csv")
-#dataMix0505 = pd.read_csv("Results\FedMIX_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.5_alphaG_0.5.csv")
-#dataMix0109 = pd.read_csv("Results\FedMIX_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.1_alphaG_0.9.csv")
-#dataMix0901 = pd.read_csv("Results\FedMIX_results\FedMix_5_local_ep_iid_1_lr_0.01_mom_0.5_epochs_50_alphaB_0.9_alphaG_0.1.csv")
-##---------- Plot the graphs ----------#
-#plt.figure(figsize=(10,10))
-#plt.plot(dataAvgBN["Epochs"],dataAvgBN["Test accuracy"],marker = "o",markersize=3.5,linestyle = "--", label = 'FedAvg IID Batch Norm')
-#plt.plot(dataAvgGN["Epochs"],dataAvgGN["Test accuracy"],marker = "o",markersize=3.5,linestyle = "--", label = 'FedAvg IID Group Norm')
-#plt.plot(dataMix0505["Epochs"],dataMix0505["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.5 alpha_g=0.5')
-#plt.plot(dataMix0109["Epochs"],dataMix0109["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.1 alpha_g=0.9')
-#plt.plot(dataMix0901["Epochs"],dataMix0901["Test accuracy"],marker = "o",markersize=3.5, label = 'FedMix IID alpha_b=0.9 alpha_g=0.1')
-#plt.ylim(bottom = 0)
-#plt.title("Comparison between methods with NonIID in FedGKT setting")
-#plt.xlabel("Communication rounds")
-#plt.ylabel("Test Accuracy")
-#plt.grid()
-#plt.legend(loc="lower right")
-#plt.show()
 
 #---------- IID----------#
 #---------- Import DataBase ----------#
@@ -155,6 +137,7 @@ plt.xlabel("Communication rounds")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with IID in FedGKT setting 128 l_bs.png")
 plt.show()
 
 
@@ -184,6 +167,7 @@ plt.xlabel("Communication rounds")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with IID in FedGKT setting 16 l_bs.png")
 plt.show()
 
 
@@ -221,6 +205,7 @@ plt.xlabel("Epochs")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with NonIID Unbalanced in FedMix settings.png")
 plt.show()
 
 
@@ -248,6 +233,7 @@ plt.xlabel("Communication rounds")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with NonIID Unbalanced in FedGKT setting 128 l_bs.png")
 plt.show()
 
 
@@ -276,4 +262,5 @@ plt.xlabel("Communication rounds")
 plt.ylabel("Test Accuracy")
 plt.grid()
 plt.legend(loc="lower right")
+plt.savefig("Images/Comparison between methods with NonIID Unbalanced in FedGKT setting 16 l_bs.png")
 plt.show()
